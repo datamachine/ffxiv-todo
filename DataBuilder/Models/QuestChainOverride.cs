42aa7d88c5e22fcef75fb1191fbd4944c7dfd52d
Vincent Castellano <vincentcastellano@gmail.com>
2026-06-14 13:05:40 -0700

    feat: add quest chain override model and empty overrides file

---
+++ DataBuilder/Models/QuestChainOverride.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace DataBuilder.Models;
+
+public sealed class QuestChainOverride
+{
+    [JsonPropertyName("contentName")]
+    public string ContentName { get; set; } = string.Empty;
+
+    [JsonPropertyName("questIds")]
+    public List<uint> QuestIds { get; set; } = new();
+}
+
+public sealed class QuestChainOverridesFile
+{
+    [JsonPropertyName("overrides")]
+    public List<QuestChainOverride> Overrides { get; set; } = new();
+}
