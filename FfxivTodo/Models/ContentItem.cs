973d243ee6023a87a5e0e85c21322c8d8d8af4d8
Vincent Castellano <vincentcastellano@gmail.com>
2026-06-14 13:03:35 -0700

    feat: add UnlockQuestIds to ContentItem model

--- FfxivTodo/Models/ContentItem.cs
+++ FfxivTodo/Models/ContentItem.cs
@@ -1,42 +1,19 @@
-using System.Text.Json.Serialization;
-
 namespace FfxivTodo.Models;
 
 public sealed class ContentItem
 {
-    [JsonPropertyName("id")]
-    public uint Id { get; init; }
-
-    [JsonPropertyName("name")]
-    public string Name { get; init; } = string.Empty;
-
-    [JsonPropertyName("level")]
-    public uint Level { get; init; }
-
-    [JsonPropertyName("expansion")]
-    public Expansion Expansion { get; init; }
-
-    [JsonPropertyName("category")]
-    public ContentCategory Category { get; init; }
-
-    [JsonPropertyName("prerequisiteIds")]
-    public uint[] PrerequisiteIds { get; init; } = [];
-
-    [JsonPropertyName("locationTerritoryId")]
-    public uint? LocationTerritoryId { get; init; }
-
-    [JsonPropertyName("locationMapX")]
-    public float? LocationMapX { get; init; }
-
-    [JsonPropertyName("locationMapY")]
-    public float? LocationMapY { get; init; }
-
-    [JsonPropertyName("questId")]
-    public uint? QuestId { get; init; }
-
-    [JsonPropertyName("achievementId")]
-    public uint? AchievementId { get; init; }
-
-    [JsonPropertyName("wikiUrl")]
-    public string? WikiUrl { get; init; }
+    public uint Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public uint Level { get; set; }
+    public Expansion Expansion { get; set; }
+    public ContentCategory Category { get; set; }
+    public uint[] PrerequisiteIds { get; set; } = [];
+    public uint? LocationTerritoryId { get; set; }
+    public string? LocationTerritoryName { get; set; }
+    public float? LocationMapX { get; set; }
+    public float? LocationMapY { get; set; }
+    public uint? QuestId { get; set; }
+    public uint? AchievementId { get; set; }
+    public uint[] UnlockQuestIds { get; set; } = [];
+    public string? WikiUrl { get; set; }
 }
